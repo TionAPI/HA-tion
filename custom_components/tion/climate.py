c4814be0cd34e0aa3d09a02565232aa7d16f6ca5
IATkachenko <IATkachenko@gmail.com>
2020-08-17 13:24:33 +0300

    Move consts to own file

--- custom_components/tion/climate.py
+++ custom_components/tion/climate.py
@@ -8,28 +8,7 @@ from tion import s3 as tion
 
 import voluptuous as vol
 
-from homeassistant.components.climate import PLATFORM_SCHEMA, ClimateEntity
-from homeassistant.components.climate.const import (
-    ATTR_PRESET_MODE,
-    CURRENT_HVAC_FAN,
-    CURRENT_HVAC_HEAT,
-    CURRENT_HVAC_OFF,
-    HVAC_MODE_HEAT,
-    HVAC_MODE_FAN_ONLY,
-    HVAC_MODE_OFF,
-    PRESET_AWAY,
-    PRESET_BOOST,
-    PRESET_NONE,
-    SUPPORT_PRESET_MODE,
-    SUPPORT_TARGET_TEMPERATURE,
-    SUPPORT_FAN_MODE,
- )
-from homeassistant.const import (
-    ATTR_TEMPERATURE,
-    CONF_NAME,
-    EVENT_HOMEASSISTANT_START,
-    PRECISION_WHOLE,
-)
+from homeassistant.components.climate import ClimateEntity
 from homeassistant.core import callback
 from homeassistant.helpers import condition
 import homeassistant.helpers.config_validation as cv
@@ -38,18 +17,9 @@ from homeassistant.helpers.event import (
     async_track_time_interval,
 )
 from homeassistant.helpers.restore_state import RestoreEntity
+from .const import *
 
 _LOGGER = logging.getLogger(__name__)
-
-DEFAULT_NAME = "Tion Breezer"
-
-CONF_TARGET_TEMP = "target_temp"
-CONF_KEEP_ALIVE = "keep_alive"
-CONF_INITIAL_HVAC_MODE = "initial_hvac_mode"
-CONF_AWAY_TEMP = "away_temp"
-CONF_MAC = "mac"
-SUPPORTED_DEVICES = ['S3']
-SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_FAN_MODE
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
---
+++ custom_components/tion/const.py
@@ -0,0 +1,33 @@
+"""Consts for Tion component"""
+from homeassistant.components.climate import PLATFORM_SCHEMA
+from homeassistant.components.climate.const import (
+    ATTR_PRESET_MODE,
+    CURRENT_HVAC_FAN,
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_OFF,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_FAN_ONLY,
+    HVAC_MODE_OFF,
+    PRESET_AWAY,
+    PRESET_BOOST,
+    PRESET_NONE,
+    SUPPORT_PRESET_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
+    SUPPORT_FAN_MODE,
+ )
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    CONF_NAME,
+    EVENT_HOMEASSISTANT_START,
+    PRECISION_WHOLE,
+)
+
+DEFAULT_NAME = "Tion Breezer"
+
+CONF_TARGET_TEMP = "target_temp"
+CONF_KEEP_ALIVE = "keep_alive"
+CONF_INITIAL_HVAC_MODE = "initial_hvac_mode"
+CONF_AWAY_TEMP = "away_temp"
+CONF_MAC = "mac"
+SUPPORTED_DEVICES = ['S3']
+SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_FAN_MODE
