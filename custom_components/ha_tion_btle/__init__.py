c6e630cb0825b5f102ff8ff1552cc0d5bef26cc6
Sergey Trofimov <trofi.mov@yandex.ru>
2025-06-17 17:42:47 +0300

    Update __init__.py (async_setup_entry function) to work on HA 2025.06+

--- custom_components/ha_tion_btle/__init__.py
+++ custom_components/ha_tion_btle/__init__.py
@@ -43,10 +43,8 @@ async def async_setup_entry(hass, config_entry: ConfigEntry):
 
     await hass.data[DOMAIN][config_entry.unique_id].async_config_entry_first_refresh()
 
-    result = True
-    for platform in PLATFORMS:
-        result = result & await hass.config_entries.async_forward_entry_setup(entry=config_entry, domain=platform)
-    return result
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    return True
 
 
 class TionInstance(DataUpdateCoordinator):
